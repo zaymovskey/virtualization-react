e2433024af66e6a2c968b5093ec153f79b531a35
zaymovskey <zaymovskey@mail.com>
2024-02-12 20:40:33 +0300

    base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const getItems = async () => Array.from({length: 500}, (_, index) => ({
+  id: Math.random().toString(36).slice(2),
+  text: String(index + 1)
+}));
+
+const itemHeight = 40;
+const containerHeight = 600;
+
 function App() {
+
+
+  const [itemsList, setItemsLits] = useState<any[]>([]);
+
+  useEffect(() => {
+    getItems().then((items) => {setItemsLits(items)});
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '0 12px', backgroundColor: "black", color: 'white' }}>
+    <h1>List</h1>
+      <div style={{ marginBottom: 12, height: containerHeight, width: 600, overflowY: 'scroll', border: '1px solid white' }}>
+        {itemsList.map((item) => (
+          <div key={item.id} style={{padding: '10px 20px'}}>{item.text}</div>
+        )) }
+      </div>
     </div>
   );
 }
